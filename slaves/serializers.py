953ba6a92e9152e320463e04a447ea2b53f6c617
ThalissonMelo <tallisonmelo46@gmail.com>
2019-04-08 14:06:18 -0300

    Adds serializers

---
+++ slaves/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from .models import Slave
+
+class SlaveSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Slave
+        fields = ('id', 'ip_address', 'location', 'transductor_list', 'url')
