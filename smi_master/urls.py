9064503ac9bfa4afe88fd38f751f4758bc94ee5f
ThalissonMelo <tallisonmelo46@gmail.com>
2019-04-08 16:10:29 -0300

    Adding campi on instaled apps

--- smi_master/urls.py
+++ smi_master/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+
+from campi.urls import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('campi.urls')),
+
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
 ]
