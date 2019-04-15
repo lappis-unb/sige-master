50b40a47eecb297ef3c5cb52b95ebd84592dd4f9
Guilherme Augusto <guilherme.francais@gmail.com>
2019-04-15 11:23:25 -0300

    Makes unique the attributes name, acronym and address

--- campi/models.py
+++ campi/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Campus(models.Model):
-    name = models.CharField(max_length=50)
-    acronym = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+    acronym = models.CharField(max_length=50, unique=True)
     phone = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
+    address = models.CharField(max_length=50, unique=True)
     website_address = models.CharField(max_length=50)
 
-    def __str__():
+    def __str__(self):
         return self.name
