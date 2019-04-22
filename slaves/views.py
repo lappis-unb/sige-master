3bfbaf244520b228d9ab09e2d25d770764a2f901
ThalissonMelo <tallisonmelo46@gmail.com>
2019-04-22 10:58:38 -0300

    Removing commented method

--- slaves/views.py
+++ slaves/views.py
@@ -3,13 +3,6 @@ from rest_framework import serializers, viewsets
 from .models import Slave
 from .serializers import SlaveSerializer
 
-
-# class SlaveView(APIView):
-#     def get(self, request):
-#         slaves = Slave.objects.all()
-#         serializer = SlaveSerializer(slaves, many=True)
-#         return Response({"slaves": slaves})
-
 class SlaveViewSet(viewsets.ModelViewSet):
     queryset = Slave.objects.all()
     serializer_class = SlaveSerializer
