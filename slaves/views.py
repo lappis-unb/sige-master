c388e4bb9ecdfcfcd0dc264850e5b0d1c4078f33
ThalissonMelo <tallisonmelo46@gmail.com>
2019-04-22 11:02:20 -0300

    Pycodestyle fix

--- slaves/views.py
+++ slaves/views.py
@@ -3,6 +3,7 @@ from rest_framework import serializers, viewsets
 from .models import Slave
 from .serializers import SlaveSerializer
 
+
 class SlaveViewSet(viewsets.ModelViewSet):
     queryset = Slave.objects.all()
     serializer_class = SlaveSerializer
