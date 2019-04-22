b18185f97ce9a0248596231a2fd95a549ae457df
Lucas Martins <lucas465@hotmail.com>
2019-04-22 12:10:14 -0300

    Changes transductor models fields

--- transductor_models/models.py
+++ transductor_models/models.py
@@ -10,30 +10,15 @@ class TransductorModel(models.Model):
         name (str): The factory name.
         transport_protocol (str): The transport protocol.
         serial_protocol (str): The serial protocol.
-        register_addresses (list): Registers with data to be collected.
-            .. note::
-            This attribute must meet the following pattern:
-
-            [[Register Address (int), Register Type (int)]]
-
-            Where:
-                - Register Address: register address itself.
-                - Register Type: register data type.
-                    - 0 - Integer
-                    - 1 - Float
-
-            Example: [[68, 0], [70, 1]]
-
-    Example of use:
-
-        >>> TransductorModel(name="Test Name", transport_protocol="UDP",
-        serial_protocol="Modbus RTU", register_addresses=[[68, 0], [70, 1]])
-        <TransductorModel: Test Name>
     """
 
     name = models.CharField(max_length=50, unique=True)
-    serial_protocol = models.CharField(max_length=50, default="")
-    transport_protocol = models.CharField(max_length=50, default="")
+    serial_protocol = models.CharField(max_length=50)
+    transport_protocol = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(TransductorModel, self).save(*args, **kwargs)
