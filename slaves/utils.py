43f13bc654118de885dd4141d746e6ffccdb4e1d
ThalissonMelo <tallisonmelo46@gmail.com>
2019-06-03 14:16:10 -0300

    Adds method to check if transductors and slaves are working

---
+++ slaves/utils.py
@@ -0,0 +1,32 @@
+from .models import Slave
+from transductors.models import EnergyTransductor
+import urllib.request
+import json
+
+
+class CheckTransductorsAndSlaves():
+
+    def check_transductors(self):
+
+        slaves = Slave.objects.all()
+
+        for slave in slaves:
+            # to work in dev dont forget to insert the port
+            url = 'http://' + slave.ip_address + '/broken_transductors'
+            web_request = urllib.request.urlopen(url)
+
+            if(web_request.status == 200):
+                data = web_request.read()
+
+                encoding = web_request.info().get_content_charset('utf-8')
+
+                transductors = json.loads(data.decode(encoding))
+
+                for transductor in transductors:
+                    transductor_status = EnergyTransductor.objects.get(
+                        serial_number=transductor['serial_number'])
+                    transductor_status.broken = transductor['broken']
+                    transductor_status.save()
+
+            else:
+                slave.broken = True
