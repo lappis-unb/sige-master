de0c8b6936015a269f9e748a1a13dae1ad56d00a
Alax Alves <alaxallves@gmail.com>
2019-10-16 14:26:36 -0300

    Deleting unwanted migrations

--- measurements/tests/test_measurements_serializer.py
+++ measurements/tests/test_measurements_serializer.py
@@ -10,7 +10,6 @@ from measurements.models import MonthlyMeasurement
 from measurements.serializers import MinutelyApparentPowerThreePhaseSerializer
 from slaves.models import Slave
 from transductors.models import EnergyTransductor
-from transductor_models.models import TransductorModel
 from datetime import datetime
 import sys
 from rest_framework.request import Request
@@ -24,18 +23,10 @@ class MeasurementsTestCase(TestCase):
             ip_address="1.1.1.1", location="UED FGA", broken=False
         )
 
-        self.transductor_model = TransductorModel.objects.create(
-            model_code='987654321', name="TR4020",
-            serial_protocol="UDP", transport_protocol="modbus",
-            minutely_register_addresses=[[1, 1]],
-            quarterly_register_addresses=[[1, 1]],
-            monthly_register_addresses=[[1, 1]]
-        )
-
         self.transductor = EnergyTransductor.objects.create(
             serial_number="12345678",
             ip_address="1.1.1.1",
-            model=self.transductor_model
+            model="MD30"
         )
 
         self.transductor.slave_servers.add(self.slave)
