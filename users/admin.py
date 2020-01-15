3d482eb87420cc638fed3d39d41a48198f58cd77
Durval Carvalho <durvalcsouza@outlook.com>
2020-01-15 19:07:13 -0300

    Remove CronJob and Grups from Django Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import *
-from .models import *
+from . import forms
+from . import models
+
+from rest_framework import authtoken
+from django_cron.models import CronJobLog
+from django.contrib.auth.models import Group
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['email', 'username']
+    add_form = forms.CustomUserCreationForm
+    form = forms.CustomUserChangeForm
+    model = models.CustomUser
+    list_display = ['email', 'username', 'user_type']
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(models.CustomUser, CustomUserAdmin)
+
+# TODO: find a better place for this
+# django_cron package by default register CronJobs on django admin
+admin.site.unregister(Group)
+admin.site.unregister(CronJobLog)
+admin.site.unregister(authtoken.models.Token)
