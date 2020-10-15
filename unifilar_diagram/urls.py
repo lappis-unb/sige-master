43f24ba67cf505ec9d090f7b5bf3066980555953
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2020-10-14 23:47:24 -0300

    Fixed lines basename url
    Co-authored-by: Andre Pinto <andrelucax@gmail.com>

--- unifilar_diagram/urls.py
+++ unifilar_diagram/urls.py
@@ -6,6 +6,6 @@ from rest_framework import routers
 app_name = "lines"
 
 router = routers.DefaultRouter()
-router.register(r'lines', LineViewSet)
+router.register(r'lines', LineViewSet, basename='lines')
 
 urlpatterns = []
