ac24a1f1379e0c4825561dedfbbd79ec8bac006b
Luciano Santos <luciano_z7@hotmail.com>
2021-04-07 22:13:11 -0300

    Improving slaves/utils.py

--- slaves/utils.py
+++ slaves/utils.py
@@ -25,7 +25,7 @@ from measurements.models import Tax
 
 class CheckTransductorsAndSlaves():
 
-    def check_transductors(self):
+    def check_slaves(self):
 
         slaves = Slave.objects.all()
 
@@ -43,11 +43,7 @@ class CheckTransductorsAndSlaves():
 
                     transductors = json.loads(data.decode(encoding))
 
-                    for transductor in transductors:
-                        transductor_status = EnergyTransductor.objects.get(
-                            id=transductor['id'])
-                        transductor_status.broken = transductor['broken']
-                        transductor_status.save()
+                    check_transductors(transductors)
 
                     slave.set_broken(False)
                 else:
@@ -56,128 +52,133 @@ class CheckTransductorsAndSlaves():
             except Exception:
                 slave.set_broken(True)
 
+    def check_transductors(self, transductors):
+        for transductor in transductors:
+            transductor_status = EnergyTransductor.objects.get(
+                id=transductor['id'])
+            transductor_status.broken = transductor['broken']
+            transductor_status.save()
+
+
 # TODO Não sabemos como resolver essa comunicação
 # Transdutores em mais de um slave tem que ser tratados de forma diferente?
 
 
 class DataCollector():
-    @staticmethod
-    def build_monthly_measurements(msm, transductor):
+
+    def build_monthly_measurements(measure, transductor):
         MonthlyMeasurement.objects.create(
-            generated_energy_peak_time=msm['generated_energy_peak_time'],
+            generated_energy_peak_time=measure['generated_energy_peak_time'],
             generated_energy_off_peak_time=(
-                msm['generated_energy_off_peak_time']
+                measure['generated_energy_off_peak_time']
             ),
-            consumption_peak_time=msm['consumption_peak_time'],
-            consumption_off_peak_time=msm['consumption_off_peak_time'],
-            inductive_power_peak_time=msm['inductive_power_peak_time'],
-            inductive_power_off_peak_time=msm['inductive_power_off_peak_time'],
-            capacitive_power_peak_time=msm['capacitive_power_peak_time'],
+            consumption_peak_time=measure['consumption_peak_time'],
+            consumption_off_peak_time=measure['consumption_off_peak_time'],
+            inductive_power_peak_time=measure['inductive_power_peak_time'],
+            inductive_power_off_peak_time=measure['inductive_power_off_peak_time'],
+            capacitive_power_peak_time=measure['capacitive_power_peak_time'],
             capacitive_power_off_peak_time=(
-                msm['capacitive_power_off_peak_time']
+                measure['capacitive_power_off_peak_time']
             ),
-            active_max_power_peak_time=msm['active_max_power_peak_time'],
+            active_max_power_peak_time=measure['active_max_power_peak_time'],
             active_max_power_off_peak_time=(
-                msm['active_max_power_off_peak_time']
+                measure['active_max_power_off_peak_time']
             ),
-            reactive_max_power_peak_time=msm['reactive_max_power_peak_time'],
+            reactive_max_power_peak_time=measure['reactive_max_power_peak_time'],
             reactive_max_power_off_peak_time=(
-                msm['reactive_max_power_off_peak_time']
+                measure['reactive_max_power_off_peak_time']
             ),
             active_max_power_list_peak_time=(
-                msm['active_max_power_list_peak_time']
+                measure['active_max_power_list_peak_time']
             ),
             active_max_power_list_peak=(
-                msm['active_max_power_list_peak']
+                measure['active_max_power_list_peak']
             ),
             active_max_power_list_off_peak_time=(
-                msm['active_max_power_list_off_peak_time']
+                measure['active_max_power_list_off_peak_time']
             ),
             active_max_power_list_off_peak=(
-                msm['active_max_power_list_off_peak']
+                measure['active_max_power_list_off_peak']
             ),
             reactive_max_power_list_peak_time=(
-                msm['reactive_max_power_list_peak_time']
+                measure['reactive_max_power_list_peak_time']
             ),
             reactive_max_power_list_peak=(
-                msm['reactive_max_power_list_peak']
+                measure['reactive_max_power_list_peak']
             ),
             reactive_max_power_list_off_peak_time=(
-                msm['reactive_max_power_list_off_peak_time']
+                measure['reactive_max_power_list_off_peak_time']
             ),
             reactive_max_power_list_off_peak=(
-                msm['reactive_max_power_list_off_peak']
+                measure['reactive_max_power_list_off_peak']
             ),
-            collection_date=msm['transductor_collection_date'],
+            collection_date=measure['transductor_collection_date'],
             transductor_id=transductor.id
         )
 
-    @staticmethod
-    def build_minutely_measurements(msm, transductor):
+
+    def build_minutely_measurements(measure, transductor):
         MinutelyMeasurement.objects.create(
-            frequency_a=msm['frequency_a'],
-            voltage_a=msm['voltage_a'],
-            voltage_b=msm['voltage_b'],
-            voltage_c=msm['voltage_c'],
-            current_b=msm['current_a'],
-            current_a=msm['current_b'],
-            current_c=msm['current_c'],
-            active_power_a=msm['active_power_a'],
-            active_power_b=msm['active_power_b'],
-            active_power_c=msm['active_power_c'],
-            total_active_power=msm['total_active_power'],
-            reactive_power_a=msm['reactive_power_a'],
-            reactive_power_b=msm['reactive_power_b'],
-            reactive_power_c=msm['reactive_power_c'],
-            total_reactive_power=msm['total_reactive_power'],
-            apparent_power_a=msm['apparent_power_a'],
-            apparent_power_b=msm['apparent_power_b'],
-            apparent_power_c=msm['apparent_power_c'],
-            total_apparent_power=msm['total_apparent_power'],
-            power_factor_a=msm['power_factor_a'],
-            power_factor_b=msm['power_factor_b'],
-            power_factor_c=msm['power_factor_c'],
-            total_power_factor=msm['total_power_factor'],
-            dht_voltage_a=msm['dht_voltage_a'],
-            dht_voltage_b=msm['dht_voltage_b'],
-            dht_voltage_c=msm['dht_voltage_c'],
-            dht_current_a=msm['dht_current_a'],
-            dht_current_b=msm['dht_current_b'],
-            dht_current_c=msm['dht_current_c'],
-            collection_date=msm['transductor_collection_date'],
+            frequency_a=measure['frequency_a'],
+            voltage_a=measure['voltage_a'],
+            voltage_b=measure['voltage_b'],
+            voltage_c=measure['voltage_c'],
+            current_b=measure['current_a'],
+            current_a=measure['current_b'],
+            current_c=measure['current_c'],
+            active_power_a=measure['active_power_a'],
+            active_power_b=measure['active_power_b'],
+            active_power_c=measure['active_power_c'],
+            total_active_power=measure['total_active_power'],
+            reactive_power_a=measure['reactive_power_a'],
+            reactive_power_b=measure['reactive_power_b'],
+            reactive_power_c=measure['reactive_power_c'],
+            total_reactive_power=measure['total_reactive_power'],
+            apparent_power_a=measure['apparent_power_a'],
+            apparent_power_b=measure['apparent_power_b'],
+            apparent_power_c=measure['apparent_power_c'],
+            total_apparent_power=measure['total_apparent_power'],
+            power_factor_a=measure['power_factor_a'],
+            power_factor_b=measure['power_factor_b'],
+            power_factor_c=measure['power_factor_c'],
+            total_power_factor=measure['total_power_factor'],
+            dht_voltage_a=measure['dht_voltage_a'],
+            dht_voltage_b=measure['dht_voltage_b'],
+            dht_voltage_c=measure['dht_voltage_c'],
+            dht_current_a=measure['dht_current_a'],
+            dht_current_b=measure['dht_current_b'],
+            dht_current_c=measure['dht_current_c'],
+            collection_date=measure['transductor_collection_date'],
             transductor_id=transductor.id
         )
 
-    @staticmethod
-    def build_quarterly_measurements(msm, transductor):
+    def build_quarterly_measurements(measure, transductor):
         QuarterlyMeasurement.objects.create(
-            generated_energy_peak_time=msm['generated_energy_peak_time'],
+            generated_energy_peak_time=measure['generated_energy_peak_time'],
             generated_energy_off_peak_time=(
-                msm['generated_energy_off_peak_time']
+                measure['generated_energy_off_peak_time']
             ),
-            consumption_peak_time=msm['consumption_peak_time'],
-            consumption_off_peak_time=msm['consumption_off_peak_time'],
-            inductive_power_peak_time=msm['inductive_power_peak_time'],
+            consumption_peak_time=measure['consumption_peak_time'],
+            consumption_off_peak_time=measure['consumption_off_peak_time'],
+            inductive_power_peak_time=measure['inductive_power_peak_time'],
             inductive_power_off_peak_time=(
-                msm['inductive_power_off_peak_time']
+                measure['inductive_power_off_peak_time']
             ),
-            capacitive_power_peak_time=msm['capacitive_power_peak_time'],
+            capacitive_power_peak_time=measure['capacitive_power_peak_time'],
             capacitive_power_off_peak_time=(
-                msm['capacitive_power_off_peak_time']
+                measure['capacitive_power_off_peak_time']
             ),
-            collection_date=msm['transductor_collection_date'],
+            collection_date=measure['transductor_collection_date'],
             transductor_id=transductor.id,
             tax=Tax.objects.last()
         )
 
-    @staticmethod
     def save_event_object(event_dict, request_type):
         """
         Builds and saves events from a dict to a given class
         """
         DataCollector.build_events(event_dict)
 
-    @staticmethod
     def build_events(events):
         for event_dict in events:
             event_class = globals()[event_dict['type']]
@@ -202,7 +203,6 @@ class DataCollector():
                 if not event_dict['ended_at']:
                     event_class().save_event(transductor, event_dict)
 
-    @staticmethod
     def get_events():
         """
         Collects all events previously created on the slave servers
@@ -215,47 +215,45 @@ class DataCollector():
                 loaded_events = json.loads(pairs[1].content)
                 DataCollector.save_event_object(loaded_events, pairs[0])
 
-    @staticmethod
-    def build_realtime_measurements(msm, transductor):
+    def build_realtime_measurements(measure, transductor):
         if RealTimeMeasurement.objects.filter(transductor=transductor):
             measurement = RealTimeMeasurement.objects.get(
                 transductor=transductor
             )
-            measurement.voltage_a = msm['voltage_a']
-            measurement.voltage_b = msm['voltage_b']
-            measurement.voltage_c = msm['voltage_c']
-            measurement.current_a = msm['current_a']
-            measurement.current_b = msm['current_b']
-            measurement.current_c = msm['current_c']
-            measurement.total_active_power = msm['total_active_power']
-            measurement.total_reactive_power = msm['total_reactive_power']
-            measurement.total_power_factor = msm['total_power_factor']
+            measurement.voltage_a = measure['voltage_a']
+            measurement.voltage_b = measure['voltage_b']
+            measurement.voltage_c = measure['voltage_c']
+            measurement.current_a = measure['current_a']
+            measurement.current_b = measure['current_b']
+            measurement.current_c = measure['current_c']
+            measurement.total_active_power = measure['total_active_power']
+            measurement.total_reactive_power = measure['total_reactive_power']
+            measurement.total_power_factor = measure['total_power_factor']
             measurement.transductor = transductor
             measurement.collection_date = datetime.strptime(
-                msm['transductor_collection_date'], 
+                measure['transductor_collection_date'],
                 '%Y-%m-%dT%H:%M:%S.%f'
             )
 
             measurement.save()
         else:
             RealTimeMeasurement.objects.create(
-                voltage_a=msm['voltage_a'],
-                voltage_b=msm['voltage_b'],
-                voltage_c=msm['voltage_c'],
-                current_a=msm['current_a'],
-                current_b=msm['current_b'],
-                current_c=msm['current_c'],
-                total_active_power=msm['total_active_power'],
-                total_reactive_power=msm['total_reactive_power'],
-                total_power_factor=msm['total_power_factor'],
+                voltage_a=measure['voltage_a'],
+                voltage_b=measure['voltage_b'],
+                voltage_c=measure['voltage_c'],
+                current_a=measure['current_a'],
+                current_b=measure['current_b'],
+                current_c=measure['current_c'],
+                total_active_power=measure['total_active_power'],
+                total_reactive_power=measure['total_reactive_power'],
+                total_power_factor=measure['total_power_factor'],
                 transductor=transductor,
                 collection_date=datetime.strptime(
-                    msm['transductor_collection_date'],
+                    measure['transductor_collection_date'],
                     '%Y-%m-%dT%H:%M:%S.%f'
                 )
             )
 
-    @staticmethod
     def get_measurements(*args, **kwargs):
         """
         Collects a given set of measurements from all slave servers
@@ -296,11 +294,11 @@ class DataCollector():
 
                     measurements = json.loads(minutely_response.content)
 
-                    for msm in measurements:
+                    for measure in measurements:
                         # Create MinutelyMeasurement object
                         try:
                             DataCollector.build_minutely_measurements(
-                                msm, transductor
+                                measure, transductor
                             )
                         except Exception as exception:
                             pass
@@ -318,11 +316,11 @@ class DataCollector():
 
                     measurements = json.loads(quarterly_response.content)
 
-                    for msm in measurements:
+                    for measure in measurements:
                         # Create QuarterlyMeasurement object
                         # try:
                         DataCollector.build_quarterly_measurements(
-                            msm, transductor
+                            measure, transductor
                         )
                         # except Exception:
                         # pass
@@ -341,11 +339,11 @@ class DataCollector():
 
                     measurements = json.loads(monthly_response.content)
 
-                    for msm in measurements:
+                    for measure in measurements:
                         # Create MonthlyMeasurement object
                         # try:
                         DataCollector.build_monthly_measurements(
-                            msm, transductor
+                            measure, transductor
                         )
                         # except Exception:
                         #     pass
