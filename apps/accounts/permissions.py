98831aa9c53df5582fcb2c50f5c2c8f3dde4477e
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-05-29 17:42:04 -0300

    lint: update linter config to exclude non-code directories and resolve unused file linting

--- apps/accounts/permissions.py
+++ apps/accounts/permissions.py
@@ -1,9 +1,6 @@
 from rest_framework import permissions
 
-from apps.accounts.models import Account
 
-
-# =================================================================================
 # Custom permissions
 class CustomUserPermissions(permissions.BasePermission):
     # def has_permission(self, request, view):
@@ -22,7 +19,6 @@ class CustomUserPermissions(permissions.BasePermission):
         return obj == user
 
 
-
 class IsAdminUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_staff
@@ -31,15 +27,6 @@ class IsAdminUser(permissions.BasePermission):
         return request.user.is_staff
 
 
-
-
-
-
-
-
-
-
-# =================================================================================
 class IsSuperUserOrAdminCreatingWithinOwnUniversity(permissions.BasePermission):
     def has_permission(self, request, view):
         # Superusuários podem criar qualquer usuário
@@ -49,11 +36,7 @@ class IsSuperUserOrAdminCreatingWithinOwnUniversity(permissions.BasePermission):
         # Administradores só podem criar usuários comuns e dentro de sua própria universidade
         if request.user.is_staff and not request.data.get("is_superuser"):
             return request.data.get("university") == request.user.university_id
-
         return False
-
-
-# =================================================================================
 
 
 class UserCreateUpdatePermission(permissions.BasePermission):
@@ -68,9 +51,6 @@ class UserCreateUpdatePermission(permissions.BasePermission):
         return not request.user.is_anonymous or "role" not in request.data and "organization" not in request.data
 
 
-# =================================================================================
-# =================================================================================
-# =================================================================================
 class IsAdminOrSameOrganization(permissions.BasePermission):
     def has_permission(self, request, view):
         # Todos os usuários autenticados podem acessar a view, mas o conteúdo será filtrado mais tarde.
@@ -82,91 +62,3 @@ class IsAdminOrSameOrganization(permissions.BasePermission):
             return True
         # Gestores só podem ver usuários na mesma organização.
         return obj.organization == request.user.organization
-
-
-# =================================================================================
-class IsUserOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return obj == request.user
-
-
-class IsUserOrReadOnlyyy(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return obj == request.user
-
-
-class IsAccountOwner(permissions.BasePermission):
-    message = "The account is not of the authenticated user"
-
-    def has_object_permission(self, request, view, obj):
-        return obj.id == request.user.id
-
-
-class IsUserOrReadOnlyy(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj == request.user
-
-
-class IsUserOwner(permissions.BasePermission):
-    message = "You must be the owner of this account."
-
-    def has_object_permission(self, request, view, obj):
-        return obj.id == request.user.id
-
-
-class CanDeleteAccount(permissions.BasePermission):
-    message = (
-        "The superuser can delete any account, the organization's admin can delete any"
-        "account in the organization, and the user can only delete their own account."
-    )
-
-    def has_permission(self, request, view):
-        return True
-
-    def has_object_permission(self, request, view, obj):
-        account = request.user
-
-        if account.is_staff:
-            return True
-        elif account.role == Account.Role.ADMIN:
-            return obj.organization == account.organization
-        elif obj == account:
-            return True
-        return False
-
-
-class CanAddOrDeleteAccount(permissions.BasePermission):
-    message = (
-        "The superuser can add or delete any account, the organization's admin can add or delete any"
-        "account in the organization, and the user can only add or delete their own account."
-    )
-
-    def has_permission(self, request, view):
-        return True
-
-    def has_object_permission(self, request, view, obj):
-        account = request.user
-
-        if account.is_staff:
-            return True
-        elif account.role == Account.Role.ADMIN:
-            return obj.organization == account.organization
-        elif obj == account:
-            return True
-        return False
