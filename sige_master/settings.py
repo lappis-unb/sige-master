583577958619b888eb41801c0ae29724cc34aa2e
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-06-04 21:56:02 -0300

    fix: correct CORS errors with library update and config changes

--- sige_master/settings.py
+++ sige_master/settings.py
@@ -72,10 +72,10 @@ if not DEBUG:
 # MIDDLEWARE
 # ------------------------------------------------------------------------------------------------
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -176,16 +176,16 @@ USE_JWT = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# CORS_ALLOWED_ORIGINS: Sequence[str]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_HEADERS = (
-    "accept",
-    "authorization",
-    "content-type",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-)
+# CORS_ALLOWED_ORIGINS: Sequence[str]
+# CORS_ALLOW_HEADERS = (
+#     "accept",
+#     "authorization",
+#     "content-type",
+#     "user-agent",
+#     "x-csrftoken",
+#     "x-requested-with",
+# )
 
 APPEND_SLASH = True
 LOGIN_REDIRECT_URL = "/"
