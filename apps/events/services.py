73696c0e6c2585ab9ec357c02c52634ec36316b8
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-06-23 21:04:41 -0300

    perf: enhance trigger and signal manager for batched operations

--- apps/events/services.py
+++ apps/events/services.py
@@ -6,88 +6,94 @@ from apps.utils.helpers import log_service
 logger = logging.getLogger("apps")
 
 
-class TriggerService:
-    def __init__(self, trigger, current_measurement):
-        self.trigger = trigger
-        self.measurement = current_measurement
-        self.field_name = trigger.field_name
-        self.field_value = getattr(current_measurement, trigger.field_name)
+class MeasurementEventManager:
+    def __init__(self, measurement, field_name):
+        self.measurement = measurement
+        self.transductor = measurement.transductor
+        self.field_name = field_name
+        self.field_value = getattr(measurement, field_name)
 
     @log_service()
-    def perform_trigger(self):
+    def perform_triggers(self, triggers):
         self.log_start()
 
         if self.field_value is None:
-            self.insert_log("Abort: Collected value is None. Trigger processing halted", level=logging.WARNING)
-            return None
+            logger.error(f"No value for field '{self.field_name}' - skipping triggers")
+            return
 
-        event = Event.objects.filter(
-            measurement_trigger=self.trigger,
-            transductor=self.measurement.transductor,
+        if not triggers.exists():
+            logger.error(f"No active triggers found for {self.measurement.__class__.__name__}")
+            return
+
+        current_event = self.get_current_event(triggers)
+        if current_event:
+            self.process_current_event(current_event, triggers)
+        else:
+            self.check_and_create_event(triggers)
+
+    def process_current_event(self, event, triggers):
+        logger.info(f"Processing existing event: {event.id}")
+        trigger = triggers.filter(id=event.trigger.id).first()
+
+        if self.is_within_threshold(trigger):
+            logger.info(f"No action taken: Event {event.id} conditions continue to be met.")
+        else:
+            self.close_event(event)
+            self.check_and_create_event(triggers)
+
+    def check_and_create_event(self, triggers):
+        logger.info("Checking and creating event...")
+        for trigger in triggers:
+            logger.info(f"Checking trigger: {trigger.id}")
+            if self.is_within_threshold(trigger):
+                self.create_event(trigger)
+                break
+            else:
+                logger.info("Trigger condition not met!")
+
+    def is_within_threshold(self, trigger):
+        if self.measurement.__class__.__name__ == "CumulativeMeasurement":
+            base_value = trigger.calculate_metric(self.transductor, self.measurement.collection_date)
+            upper_threshold = trigger.calculate_threshold(base_value, trigger.upper_threshold_percent)
+            lower_threshold = trigger.calculate_threshold(base_value, trigger.lower_threshold_percent)
+        elif self.measurement.__class__.__name__ == "InstantMeasurement":
+            lower_threshold = trigger.lower_threshold if trigger.lower_threshold is not None else float("-inf")
+            upper_threshold = trigger.upper_threshold if trigger.upper_threshold is not None else float("inf")
+
+        is_within = lower_threshold <= self.field_value < upper_threshold
+        logger.info(f"Conditions: {lower_threshold:.3f} <= {self.field_value} < {upper_threshold:.3f} = {is_within}")
+        return is_within
+
+    def create_event(self, trigger):
+        new_event = Event.objects.create(
+            trigger=trigger,
+            transductor=self.transductor,
             is_active=True,
-        ).first()
-
-        if event.exists():
-            self.insert_log("Found existing event:")
-            self.process_existing_event(event)
-        else:
-            self.insert_log("No active event found, creating new event.")
-            self.process_no_existing_event()
-
-    def process_existing_event(self, event):
-        self.insert_log(f"Processing existing event: {event.id}")
-        threshold = self.trigger.deactivate_threshold or self.trigger.active_threshold
-
-        if self.meets_trigger_condition(threshold):
-            self.insert_log("No action taken: event remains active as conditions are not met.")
-        else:
-            event.close_event()
-            self.insert_log(f"Event {event} closed: condition met.")
-
-    def process_no_existing_event(self):
-        if self.meets_trigger_condition(self.trigger.active_threshold):
-            new_event = Event.objects.create(
-                measurement_trigger=self.trigger,
-                transductor=self.measurement.transductor,
-                event_type=self.trigger.event_type,
-                is_active=True,
-            )
-            self.insert_log(f"New event created: {new_event}, condition met.")
-        else:
-            self.insert_log("Condition not met: no new event created.")
-
-    def meets_trigger_condition(self, threshold):
-        operator_functions = {
-            "gt": lambda x, y: x > y,
-            "gte": lambda x, y: x >= y,
-            "lt": lambda x, y: x < y,
-            "lte": lambda x, y: x <= y,
-            "exact": lambda x, y: x == y,
-            "ne": lambda x, y: x != y,
-        }
-
-        if self.trigger.operator not in operator_functions:
-            self.insert_log(f"Error: Invalid operator '{self.trigger.operator}' used.", level=logging.ERROR)
-            raise ValueError("Invalid operator")
-
-        result = operator_functions[self.trigger.operator](self.field_value, threshold)
-        self.insert_log(
-            f"Condition check: {self.field_value} {self.trigger.operator} {threshold} = {result}",
-            level=logging.DEBUG,
         )
-        return result
+        logger.info(f"New event created: {new_event.id}, for trigger: {trigger.id},  field: {self.field_name}.")
+        return new_event
+
+    def close_event(self, event):
+        event.close_event()
+        logger.info(f"Event {event.id} closed: conditions no longer met!")
+
+    def get_current_event(self, triggers):
+        events = Event.objects.filter(
+            trigger__in=triggers,
+            transductor=self.transductor,
+            is_active=True,
+        )
+
+        if events.count() > 1:
+            error_msg = f"Abort: More than one active event found: {self.transductor} - {self.field_name}"
+            logger.error(error_msg)
+            raise ValueError(error_msg)
+        return events.first()
 
     def log_start(self):
-        logger.info("-" * 85)
-        logger.info(f"=> Starting Perform trigger {self.trigger.id} - {self.measurement.__class__.__name__}")
-        logger.debug(f"   Transductor: {self.measurement.transductor}")
-        logger.debug(f"   Measurement: {self.measurement.id}")
+        logger.info("-" * 65)
+        logger.info(f"=> Starting Perform triggers {self.measurement.__class__.__name__}")
+        logger.debug(f"   Transductor: {self.transductor}")
         logger.debug(f"   Field name:  {self.field_name}")
         logger.debug(f"   Field value: {self.field_value}")
-        logger.info("-" * 85)
-
-    def add_log(self, message, level=logging.INFO, raise_exception=False):
-        logger.log(level, f"Trigger {self.trigger.id}: {message}")
-
-        if raise_exception:
-            raise ValueError(message)
+        logger.info("-" * 65)
--- apps/events/signals.py
+++ apps/events/signals.py
@@ -4,37 +4,56 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.events.models import CumulativeMeasurementTrigger, InstantMeasurementTrigger
-from apps.events.services import TriggerService
+from apps.events.services import MeasurementEventManager
 from apps.measurements.models import CumulativeMeasurement, InstantMeasurement
 
 logger = logging.getLogger("apps")
 
 
-@receiver(post_save, sender=CumulativeMeasurement)
-def handle_cumulative_trigger(sender, instance, created, **kwargs):
-    if created:
-        logger.info("Cumulative measurement created")
-        triggers = CumulativeMeasurementTrigger.objects.filter(is_active=True)
-        if not triggers:
-            logger.info("No active triggers found for Cumulative measurement")
-            return
-
-        logger.info(f"Processing triggers for {instance.transductor.ip_address}")
-        for trigger in triggers:
-            trigger_service = TriggerService(trigger, instance)
-            trigger_service.perform_trigger()
-
-
 @receiver(post_save, sender=InstantMeasurement)
 def handle_instant_trigger(sender, instance, created, **kwargs):
-    if created:
-        logger.info("Instant measurement created")
-        triggers = InstantMeasurementTrigger.objects.filter(is_active=True)
-        if not triggers:
-            logger.info("No active triggers found for Instant measurement")
-            return
+    if not created:
+        return
+    logger.debug("Instant measurement created")
+    active_triggers = InstantMeasurementTrigger.objects.filter(is_active=True)
+    if not active_triggers.exists():
+        logger.debug("No active triggers found for Instant measurement")
+        return
 
-        logger.info(f"Processing triggers for {instance.transductor.ip_address}")
-        for trigger in triggers:
-            trigger_service = TriggerService(trigger, instance)
-            trigger_service.perform_trigger()
+    field_names = active_triggers.values_list("field_name", flat=True).distinct()
+    for field_name in field_names:
+        field_triggers = active_triggers.filter(field_name=field_name)
+
+        field_value = getattr(instance, field_name, None)
+        if field_value is None:
+            logger.debug(f"No value for field '{field_name}' - skipping triggers")
+            continue
+
+        event_manager = MeasurementEventManager(instance, field_name)
+        event_manager.perform_triggers(field_triggers)
+
+
+@receiver(post_save, sender=CumulativeMeasurement)
+def handle_cumulative_trigger(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    logger.debug("Cumulative measurement created")
+    active_triggers = CumulativeMeasurementTrigger.objects.filter(is_active=True)
+    if not active_triggers.exists():
+        logger.debug("No active triggers found for Cumulative measurement")
+        return
+
+    field_names = active_triggers.values_list("field_name", flat=True).distinct()
+    for field_name in field_names:
+        field_triggers = active_triggers.filter(field_name=field_name)
+
+        field_value = getattr(instance, field_name, None)
+        if field_value is None:
+            logger.debug(f"No value for field '{field_name}' - skipping triggers")
+            continue
+
+        logger.debug(f"Processing triggers for {field_name} for {instance.transductor.ip_address}")
+
+        event_manager = MeasurementEventManager(instance, field_name)
+        event_manager.perform_triggers(field_triggers)
