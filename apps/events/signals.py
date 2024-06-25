ab2eab7013e6ef14a3810f18d78b353a254f7c3f
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-06-25 02:25:34 -0300

    feat: add signal for monitoring state changes in transducers

--- apps/events/signals.py
+++ apps/events/signals.py
@@ -3,9 +3,15 @@ import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from apps.events.models import CumulativeMeasurementTrigger, InstantMeasurementTrigger
+from apps.events.models import (
+    CumulativeMeasurementTrigger,
+    Event,
+    InstantMeasurementTrigger,
+    TransductorStatusTrigger,
+)
 from apps.events.services import MeasurementEventManager
 from apps.measurements.models import CumulativeMeasurement, InstantMeasurement
+from apps.transductors.models import StatusHistory
 
 logger = logging.getLogger("apps")
 
@@ -57,3 +63,35 @@ def handle_cumulative_trigger(sender, instance, created, **kwargs):
 
         event_manager = MeasurementEventManager(instance, field_name)
         event_manager.perform_triggers(field_triggers)
+
+
+@receiver(post_save, sender=StatusHistory)
+def handle_status_history(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    logger.debug("Status history created")
+    active_triggers = TransductorStatusTrigger.objects.filter(is_active=True)
+    if not active_triggers.exists():
+        logger.debug("No active triggers found for Status history")
+        return
+
+    previous_instance = (
+        StatusHistory.objects.filter(transductor=instance.transductor)  #
+        .exclude(pk=instance.pk)
+        .order_by("-id")
+        .first()
+    )
+
+    for trigger in active_triggers:
+        logger.debug(f"Trigger target status: {trigger.get_target_status_display()}")
+
+        if previous_instance.status == trigger.target_status:
+            logger.info(f"Previous status matches trigger target status: {trigger.get_target_status_display()}")
+            event = Event.objects.filter(transductor=instance.transductor, trigger=trigger, is_active=True).first()
+
+            if event:
+                event.close_event()
+                logger.info(f"Event {event.id} closed: conditions no longer met!")
+            else:
+                logger.info(f"No active event found for trigger on {instance.transductor}")
