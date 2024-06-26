b0310d69a4261440893add2cf536eb1938f96b00
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-06-26 04:22:15 -0300

    feat: create endpoint for generating transductors status summaries

--- apps/transductors/views.py
+++ apps/transductors/views.py
@@ -1,16 +1,29 @@
 import logging
 
-from rest_framework import viewsets
+from django.db.models import Count, Q
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from apps.transductors.models import StatusHistory, Transductor, TransductorModel
+from apps.events.models import Event
+from apps.events.pagination import EventPagination
+from apps.events.serializers import EventSerializer
+from apps.events.services import calculate_aggregation_events
+from apps.transductors.models import (
+    Status,
+    StatusHistory,
+    Transductor,
+    TransductorModel,
+)
 from apps.transductors.serializers import (
     TransductorCreateSerializer,
     TransductorDetailSerializer,
+    TransductorListSerializer,
     TransductorModelSerializer,
     TransductorStatusDetailSerializer,
     TransductorStatusSerializer,
 )
+from apps.transductors.services import calculate_aggregation_status
 from apps.utils.helpers import get_boolean
 
 logger = logging.getLogger("apps.transductors.views")
@@ -25,7 +38,9 @@ class TransductorViewSet(viewsets.ModelViewSet):
     queryset = Transductor.objects.all()
 
     def get_serializer_class(self):
-        if self.action in ["list", "retrive"]:
+        if self.action == "list":
+            return TransductorListSerializer
+        if self.action == "retrieve":
             return TransductorDetailSerializer
         else:
             return TransductorCreateSerializer
@@ -52,6 +67,31 @@ class TransductorStatusViewSet(viewsets.ModelViewSet):
     # permission_classes = [CurrentADMINUserOnly]
 
     def get_serializer_class(self):
-        if self.action in ["list", "retrive"]:
+        if self.action in ["list", "retrieve"]:
             return TransductorStatusDetailSerializer
         return TransductorStatusSerializer
+
+    @action(methods=["get"], detail=False)
+    def summary(self, request):
+        queryset = self.get_queryset()
+
+        general_aggregation = queryset.aggregate(
+            total_active=Count("id", filter=Q(status=Status.ACTIVE)),
+            total_broken=Count("id", filter=Q(status=Status.BROKEN)),
+            total_disabled=Count("id", filter=Q(status=Status.DISABLED)),
+            total_maintenance=Count("id", filter=Q(status=Status.MAINTENANCE)),
+            total_other=Count("id", filter=Q(status=Status.OTHER)),
+        )
+
+        transductors_summary = []
+        transductors_qs = Transductor.objects.all().prefetch_related("status_history")
+        for transductor in transductors_qs:
+            status_history_qs = StatusHistory.objects.filter(transductor=transductor)
+            transductor_status_summary = calculate_aggregation_status(status_history_qs, transductor)
+            transductors_summary.append(transductor_status_summary)
+
+        response_data = {
+            "general_status_summary": general_aggregation,
+            "transductors_status_summary": transductors_summary,
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
