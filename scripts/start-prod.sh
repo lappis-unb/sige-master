580f6959f4332333fe7606094d19b2207eb004aa
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-08-14 12:18:03 -0300

    feat: update script to run django in production with gunicorn

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -1,27 +1,75 @@
 #!/bin/bash
+set -e errexit
+set -e pipefail
+set -e nounsetcle
+set -e xtrace
+
+C='\e[0;36m'     # cyan
+Y='\033[0;33m'   # yellow
+G='\033[0;32m'   # green 
+B='\033[0;34m'   # blue
+W='\033[0;37m'   # white
+E='\033[0m'      # end
 
 # Exporting all environment variables to use in crontab
 env | sed 's/^\(.*\)$/ \1/g' > /root/env
 
-echo '======= CHECKING FOR UNINSTALLED PKGs AND INSTALLING'
-pip freeze || pip install -r requirements.txt
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+echo "${C}=> WAIT POSTGRES DB TO BE READY"
+sleep 3 # wait 5 seconds initial startup before starting the server
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q'; do
+  >&2 echo "\n${Y}Postgres is unavailable - sleeping...\n"
+  sleep 1
+done
+echo
+echo "${G}Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
-echo "======= POSTGRES IS UP, CONNECTING"
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+echo "${C}=> CREATING LOGS DIRECTORY                                                                                                  ${E}"
+mkdir -p /sige-master/logs/tasks
+mkdir -p /sige-master/logs/apps
+chmod -R 777 /sige-master/logs
 
-echo '======= RUNNING PIP INSTALL'
-pip install --no-cache-dir -r requirements.txt
+if [ -d "/sige-master/logs" ]; then
+  echo "- Tasks directory: /sige-master/logs/tasks"
+  echo "- Apps directory: /sige-master/logs/apps"
+  echo "${G}Logs directory created successfully!"
+else
+  echo "${Y}Logs directory not created!"
+fi
 
-echo '======= MAKING MIGRATIONS'
-python3 manage.py makemigrations
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+echo "${C}=> CHECK INTEGRITY (check)                                                                                                  ${E}"
+python manage.py check
+                                                                                                       
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+echo "${C}=> APPLYING MIGRATIONS (migrate)                                                                                            ${E}"
+python manage.py migrate --noinput
 
-echo '======= RUNNING MIGRATIONS'
-python3 manage.py migrate
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+echo "${C}=> POPULATING DATABASE (custom command)                                                                                           ${E}"
+python manage.py populate_states
 
-echo '======= RUNNING SEED'
-python3 seed_db.py
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+echo "${C}=> COLLECTING STATIC FILES (collectstatic)                                                                                 ${E}"
+python manage.py collectstatic --noinput
 
-echo '======= STARTING CRON'
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+echo "${C}=> STARTING CRON                                                                                                            ${E}"
+echo 'Cronjobs sige-cron in operating system'
+/bin/cp /sige-master/crons/cronjob /etc/cron.d/sige-cron
+crontab /etc/cron.d/sige-cron
 cron
+cronitor list /etc/cron.d/sige-cron
 
-echo '======= RUNNING SERVER'
-python3 manage.py runserver 0.0.0.0:8001
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+echo '\e[33;36m=> RUNNING SERVER (gunicorn) \e[0;33m                                                                            _        '
+echo '\e[1;33m                                                       ۞          ___ _  __ _  ___    _ __ ___   __ _ ___| |_ ___ _ __     '
+echo '\e[1;33m                                                     ۞   ۞       / __| |/ _` |/ _ \  | `_ ` _ \ / _` / __| __/ _ \ `__|    '
+echo '\e[1;33m                                                   ۞   ۞   ۞     \__ \ | (_| |  __/  | | | | | | (_| \__ \ ||  __/ |      '
+echo '\e[1;33m                                                     ۞   ۞       |___/_|\__, |\___|  |_| |_| |_|\__,_|___/\__\___|_|       '
+echo '\e[1;33m                                                       ۞                |___/                                              ' 
+echo "Starting ${W}"${API_NAME}"${E} - ${G}http://"${API_HOST}":${API_PORT}${E}                                             ${B}UnB ${Y}Energia   "
+echo "         ${W}${POSTGRES_HOST}${E}  - ${Y}http://"${API_HOST}":${POSTGRES_PORT}${E}                                                      ${B}Eficiencia Energetica"
+echo "${C}____________________________________________________________________________________________________________________________${E}"
+/usr/local/bin/gunicorn --bind "0.0.0.0:${API_PORT}" sige_master.wsgi:application --workers 4 --timeout 120
--- sige_master/wsgi.py
+++ sige_master/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sige_master.settings")
+environment = os.getenv("ENVIRONMENT", "production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"sige_master.settings.{environment}")
 
 application = get_wsgi_application()
